3ab87a770ab83721c6ed1d4e096b5daee27a930b
Robin Chatelet <robinchatelet21@gmail.com>
2017-01-03 15:45:07 +0100

    Update segmentation.ml

--- segmentation.ml
+++ segmentation.ml
@@ -1,1 +1,8 @@
+(*------------------------Segmentation------------------------------*)
 
+let blue = {r=0 ; g= 0 ;b=255};;
+let red = {r=255 ; g=0 ; b=0};;
+let green = {r=0 ; g=255 ; b=0};;
+
+let compare 
+let distance

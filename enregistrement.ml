dbc4a8bbc88b1306f353312fe7254994560ada07
Robin Chatelet <robinchatelet21@gmail.com>
2017-01-04 15:16:31 +0100

    Update enregistrement.ml

--- enregistrement.ml
+++ enregistrement.ml
@@ -1,5 +1,15 @@
 (*------------------------ Enregistrement ------------------------------*)
 
-generer matrice couleur
+let supprime_element i l = if i < 0 || i>= List.length l then failwith
+                           else 
+                              let rec delete i l = match l with 
+                                          [] -> []
+                                          |h::t when i = 0 -> t
+                                          |h::t -> h::delete(i-1) t
+                                in delete i l;;
+                                
+let rec fusion m1 m2 n = match m2 with
+[]-> []
+|_ -> 
 
 

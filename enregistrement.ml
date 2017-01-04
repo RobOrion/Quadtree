b2760e7362a0f7cd9228b35b4c148fe78995c815
Robin Chatelet <robinchatelet21@gmail.com>
2017-01-04 15:37:46 +0100

    Update enregistrement.ml

--- enregistrement.ml
+++ enregistrement.ml
@@ -9,7 +9,20 @@ let supprime_element i l = if i < 0 || i>= List.length l then failwith
                                 in delete i l;;
                                 
 let rec fusion m1 m2 n = match m2 with
-[]-> []
-|_ -> 
+              []-> []
+              |_ -> ((List.nth m1 n)@(List.nth m2 0))::(fusion m1 (supprime 0 m2) (n+1));;
+              
+let creerMat a = [[a]];;
+
+let rec transfoArbre a = match a with
+              Nul -> []
+              |Noeud(v,1,Nul,Nul,Nul) -> creerMat(v)
+              |Noeud(v,f1,f2,f3,f4) -> fusion ((transfoArbre f1)@(transfoArbre f3)) ((transfoArbre f2)(transfoArbre f4)) 0;;
+              
+let PixeltoString pixel = (string_of_int pixel.r)^" "^(string_of_int pixel.g)^" "^(string_of_int pixel.b)^" ";;
+
+
+
+
 
 
